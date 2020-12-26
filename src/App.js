acd1772f85084aa8aad173909f4e722a2c607e29
Piotr Rześny <rzesnypPiotrr@gmail.com>
2020-12-26 18:40:42 +0100

    router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './styles/App.css';
 import Header from './compo/Header';
 import Sidebar from './compo/Sidebar';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 const App = () => {
 	return (
-		<div className='app'>
-			<Header />
-			<Sidebar />
-		</div>
+		<Router>
+			<div className='app'>
+				<Header />
+				<div className='app__body'>
+					<Sidebar />
+
+					<Switch></Switch>
+				</div>
+			</div>
+		</Router>
 	);
 };
 
