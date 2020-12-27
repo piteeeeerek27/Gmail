d4e7d4fb8c6c8eca07d6a40b039a308ddeb33a2d
Piotr Rześny <rzesnypPiotrr@gmail.com>
2020-12-27 20:53:41 +0100

    mail end

--- src/compo/EmailRow.js
+++ src/compo/EmailRow.js
@@ -1,15 +1,33 @@
-import { Checkbox, IconButton } from '@material-ui/core';
-import React from 'react';
-import '../styles/EmailRow.css';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import LabelImportantIcon from '@material-ui/icons/LabelImportant';
-import { useHistory } from 'react-router-dom';
+import { Checkbox, IconButton } from "@material-ui/core";
+import React from "react";
+import "../styles/EmailRow.css";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { selectMail } from "../features/mailSlice";
 
 function EmailRow({ title, subject, description, time, id }) {
 	const history = useHistory();
+	const dispatch = useDispatch();
+
+	const openMail = () => {
+		dispatch(
+			selectMail({
+				title,
+				subject,
+				description,
+				time,
+				id,
+			}),
+		);
+
+		history.push("./mail");
+	};
+
 	return (
-		<div onClick={() => history.push('/mail')} className='emailRow'>
-			<div className='emailRow__options'>
+		<div onClick={openMail} className="emailRow">
+			<div className="emailRow__options">
 				<Checkbox />
 				<IconButton>
 					<StarBorderIcon />
@@ -19,16 +37,16 @@ function EmailRow({ title, subject, description, time, id }) {
 				</IconButton>
 			</div>
 
-			<h3 className='emailRow__title'>{title}</h3>
+			<h3 className="emailRow__title">{title}</h3>
 
-			<div className='emailRow__message'>
+			<div className="emailRow__message">
 				<h4>
-					{subject} {' - '}{' '}
-					<span className='emailRow__description'>{description}</span>
+					{subject} {" - "}{" "}
+					<span className="emailRow__description">{description}</span>
 				</h4>
 			</div>
 
-			<p className='emailRow__description'>{time}</p>
+			<p className="emailRow__description">{time}</p>
 		</div>
 	);
 }
--- src/compo/Mail.js
+++ src/compo/Mail.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import '../styles/Mail.css';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import MoveToInboxIcon from '@material-ui/icons/MoveToInbox';
-import ErrorIcon from '@material-ui/icons/Error';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EmailIcon from '@material-ui/icons/Email';
-import WatchLaterIcon from '@material-ui/icons/WatchLater';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import LabelImportantIcon from '@material-ui/icons/LabelImportant';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import UnfoldMoreIcon from '@material-ui/icons/UnfoldMore';
-import PrintIcon from '@material-ui/icons/Print';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import { IconButton } from '@material-ui/core';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import "../styles/Mail.css";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import MoveToInboxIcon from "@material-ui/icons/MoveToInbox";
+import ErrorIcon from "@material-ui/icons/Error";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EmailIcon from "@material-ui/icons/Email";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
+import PrintIcon from "@material-ui/icons/Print";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { IconButton } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { selectOpenMail } from "../features/mailSlice";
+import { useSelector } from "react-redux";
 
 const Mail = () => {
 	const history = useHistory();
+	const selectedMail = useSelector(selectOpenMail);
+
 	return (
-		<div className='mail'>
-			<div className='mail__tools'>
-				<div className='mail__toolsLeft'>
-					<IconButton onClick={() => history.push('/')}>
+		<div className="mail">
+			<div className="mail__tools">
+				<div className="mail__toolsLeft">
+					<IconButton onClick={() => history.push("/")}>
 						<ArrowBackIcon />
 					</IconButton>
 					<IconButton>
@@ -50,7 +54,7 @@ const Mail = () => {
 					</IconButton>
 				</div>
 
-				<div className='mail__toolsRight'>
+				<div className="mail__toolsRight">
 					<IconButton>
 						<UnfoldMoreIcon />
 					</IconButton>
@@ -64,16 +68,16 @@ const Mail = () => {
 					</IconButton>
 				</div>
 			</div>
-			<div className='mail__body'>
-				<div className='mail__bodyHeader'>
-					<h2>Subject</h2>
-					<LabelImportantIcon className='mail__important' />
-					<p>Title</p>
-					<p className='mail__time'>10pm</p>
+			<div className="mail__body">
+				<div className="mail__bodyHeader">
+					<h2>{selectedMail?.subject}</h2>
+					<LabelImportantIcon className="mail__important" />
+					<p>{selectedMail?.title}</p>
+					<p className="mail__time">{selectedMail?.time}</p>
 				</div>
 
-				<div className='mail__message'>
-					<p>This is a message</p>
+				<div className="mail__message">
+					<p>{selectedMail?.description}</p>
 				</div>
 			</div>
 		</div>
